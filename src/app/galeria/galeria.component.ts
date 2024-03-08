ca6866398474689055f0d045b07de3b2829f982d
DidacusZ <diegoooooooooooooooo16@gmail.com>
2024-03-08 13:05:52 +0100

    fin 1.0.3 eliminar img

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage';
+import { Storage, ref, uploadBytes, listAll, getDownloadURL, deleteObject } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-galeria',
@@ -26,19 +26,34 @@ export class GaleriaComponent implements OnInit {
     }).catch(error => console.log(error));
   } 
 
-   getImages() {
+  getImages() {
     const usuarioId = localStorage.getItem('idUsuario');
     const imagesRef = ref(this.storage, `${usuarioId}`);
 
     listAll(imagesRef).then(async images => {
-      this.images = [];
-      for(let image of images.items) {
-        const url = await getDownloadURL(image);
-        this.images.push(url);
-      }
+    this.images = [];
+    for(let image of images.items) {
+      const url = await getDownloadURL(image);
+      this.images.push(url);
+    }
 
     }).catch(error => console.log(error));
   };
 
+  eliminarArchivo(ruta:string){
+    if(confirm("¿Seguro que quieres eliminar esta imagen?")){
+
+        const referencia = ref(this.storage, ruta);
+
+        deleteObject(referencia).then(() => {          
+          alert("Imagen borrada correctamente")
+          window.location.reload();
+        }).catch((error) => {
+          alert("ERROR la Imagen no se ha borrada")
+        });
+
+    }
+
+  }
 
 }
